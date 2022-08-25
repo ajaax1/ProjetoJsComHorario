d898329b9520b0d8420bf0fcb49ce9c557a2418b
Ruan Higor Silva <ruanhigor123@gmail.com>
2022-08-25 16:55:56 -0300

    Projeto para praticar utilizando o horário do dia

---
+++ java.js
@@ -0,0 +1,30 @@
+function carregar(){
+
+    let data = new Date()
+    let datas =data.getHours()
+    document.getElementById(imagem)
+    let msg =  window.document.getElementById("titulo")
+    msg.innerHTML = `Agora são ${datas} Horas`
+
+    if (datas>=6 && datas<13){
+        document.getElementById("myImageId").src="manha.jpg";
+        document.getElementById("titulo2").innerHTML = "manha"
+        
+    }
+    else if (datas>=13 && datas<18){
+        document.getElementById("myImageId").src="tarde.jpg";
+        document.querySelector("#titulo2").innerHTML= "tarde";
+    }
+    else if (datas>=19 && datas<23){
+        document.getElementById("myImageId").src="noite.jpg"
+        document.getElementById("titulo2").innerHTML= "noite"
+    }
+    else if (datas>=23 && datas<=5){
+        document.getElementById("myImageId").src="noite.jpg"
+        document.getElementById("titulo2").innerHTML= "noite"
+    }
+    
+}
+
+
+
