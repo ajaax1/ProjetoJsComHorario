c88e0b3d08eee96df093b5a03819bc297a524465
Ruan Higor Silva <ruanhigor123@gmail.com>
2022-08-25 23:41:10 -0300

    atualização

--- java.js
+++ java.js
@@ -7,20 +7,20 @@ function carregar(){
     msg.innerHTML = `Agora são ${datas} Horas`
 
     if (datas>=6 && datas<13){
-        document.getElementById("myImageId").src="manha.jpg";
+        document.getElementById("myImageId").src="imagens/manha.jpg";
         document.getElementById("titulo2").innerHTML = "manha"
         
     }
     else if (datas>=13 && datas<18){
-        document.getElementById("myImageId").src="tarde.jpg";
+        document.getElementById("myImageId").src="imagens/tarde.jpg";
         document.querySelector("#titulo2").innerHTML= "tarde";
     }
-    else if (datas>=19 && datas<23){
-        document.getElementById("myImageId").src="noite.jpg"
+    else if (datas>=19 && datas<=23){
+        document.getElementById("myImageId").src="imagens/noite.jpg"
         document.getElementById("titulo2").innerHTML= "noite"
     }
-    else if (datas>=23 && datas<=5){
-        document.getElementById("myImageId").src="noite.jpg"
+    else if (datas>=00 && datas<=5){
+        document.getElementById("myImageId").src="imagens/noite.jpg"
         document.getElementById("titulo2").innerHTML= "noite"
     }
     
